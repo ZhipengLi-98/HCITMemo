4fe54df929f4851068ffe6d54c89d3cce3231173
arsenalahm <clevercake7@gmail.com>
2021-05-12 00:25:49 +0800

    Add laser and fridge mark

---
+++ Assets/Scripts/LaserManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserManager : MonoBehaviour
+{
+    public LineRenderer laserLineRenderer;
+    public float laserWidth = 0.01f;
+    public OVRInput.Controller controllerType;
+    public float dist;
+    private Vector3 startPoint;
+    public GameObject centerEye;
+    public GameObject textFridge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserLineRenderer.positionCount = 2;
+        laserLineRenderer.SetPosition(0, Vector3.zero);
+        laserLineRenderer.SetPosition(1, Vector3.zero);
+        laserLineRenderer.startWidth = laserWidth;
+        laserLineRenderer.endWidth = laserWidth;
+        dist = 10.0f;
+        startPoint = new Vector3(0, 1.5f, 0);
+        textFridge.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetActiveController() == controllerType)
+        {
+            startPoint = centerEye.transform.position;
+            laserLineRenderer.SetPosition(0, startPoint + OVRInput.GetLocalControllerPosition(controllerType));
+            laserLineRenderer.SetPosition(1, startPoint + OVRInput.GetLocalControllerPosition(controllerType) + dist * (OVRInput.GetLocalControllerRotation(controllerType) * Vector3.forward));
+
+            RaycastHit hit;
+            if (Physics.Raycast(startPoint + OVRInput.GetLocalControllerPosition(controllerType), startPoint + OVRInput.GetLocalControllerPosition(controllerType) + dist * (OVRInput.GetLocalControllerRotation(controllerType) * Vector3.forward), out hit, 8f))
+            {
+                if (hit.collider != null)
+                {
+                    if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+                    {
+                        if (hit.transform.gameObject.name.Equals("PFB_Fridge"))
+                        {
+                            Debug.Log(hit.transform.gameObject.name);
+                            textFridge.SetActive(true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveManager : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject centereye;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        float x = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
+        float y = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y;
+        cam.transform.position += (x / 10) * (centereye.transform.right);
+        cam.transform.position += (y / 10) * (centereye.transform.forward);
+    }
+}
